d18a60224377059a8cc15b2ee1f74c59cddc6c6a
Aakash <aakash.ohri19@gmail.com>
2020-02-18 19:35:43 +0530

    Query done

---
+++ src/data.js
@@ -0,0 +1,157 @@
+const users = [
+    {
+      id: 'AJD064',
+      name: 'Aakash',
+      age: 30,
+      gpa: 3.2,
+      isEmployed: true,
+      comments: [],
+    },
+    {
+      id: 'AJD874',
+      name: 'Joey',
+      age: 50,
+      gpa: 9,
+      isEmployed: false,
+      comments: ['C-0945', 'C-09452', 'C-09453'],
+    },
+    {
+      id: 'AJD862',
+      name: 'Chandler',
+      age: 45,
+      gpa: null,
+      isEmployed: true,
+      comments: ['C-996311', 'C-99206', 'C-992069', 'C-992068'],
+    },
+    {
+      id: 'AJD864',
+      name: 'Monica',
+      age: 40,
+      gpa: 3.2,
+      isEmployed: false,
+      comments: ['C-2', 'C-1', 'C-3'],
+    },
+  ];
+  
+  const posts = [
+    {
+      id: 'AJDK98',
+      title: 'Test Post 1',
+      body: 'Body of pOst 1',
+      published: true,
+      author: 'AJD862',
+      comments: ['C-1', 'C-992067', 'C-992068', 'C-992069']
+    },
+    {
+      id: 'AJDK9534',
+      title: 'Test Post 2',
+      body: 'Body of pOst 1',
+      published: true,
+      author: 'AJD862',
+      comments: ['C-2', 'C-874514', 'C-99206']
+    },
+    {
+      id: 'AJDK98u38',
+      title: 'Sample Post 1',
+      body: 'Body of pOst 2',
+      published: true,
+      author: 'AJD874',
+      comments: ['C-3', 'C-874513', 'C-996311']
+    },
+    {
+      id: 'AJDK98724c',
+      title: 'Sample Post 2',
+      body: 'Body of pOst 4',
+      published: true,
+      author: 'AJD064',
+      comments: ['C-1', 'C-0945', 'C-09452', 'C-09453', 'C-874512']
+    },
+  ];
+  
+  const comments = [
+    {
+      id: 'C-1',
+      text: 'Joey is a Rockstar 1',
+      author: 'AJD864',
+      post: 'AJDK98'
+    },
+    {
+      id: 'C-2',
+      text: 'Joey is a Rockstar 1',
+      author: 'AJD864',
+      post: 'AJDK9534',
+    },
+    {
+      id: 'C-3',
+      text: 'Joey is a Rockstar 1',
+      author: 'AJD864',
+      post: 'AJDK98u38',
+    },
+    {
+      id: 'C-0945',
+      text: 'Joey is a Rockstar 1',
+      author: 'AJD874',
+      post: 'AJDK98724c'
+    },
+    {
+      id: 'C-09452',
+      text: 'Joey is a Rockstar 2',
+      author: 'AJD874',
+      post: 'AJDK98724c'
+    },
+    {
+      id: 'C-09453',
+      text: 'Joey is a Rockstar 3',
+      author: 'AJD874',
+      post: 'AJDK98'
+    },
+  
+    {
+      id: 'C-874512',
+      text: 'Chandler is a Bae',
+      author: 'AJD064',
+      post: 'AJDK98'
+    },
+    {
+      id: 'C-874513',
+      text: 'Chandler is a Bae',
+      author: 'AJD064',
+    },
+    {
+      id: 'C-874514',
+      text: 'Chandler is a Bae',
+      author: 'AJD064',
+    },
+    {
+      id: 'C-992067',
+      text: 'Charlie wins ALL',
+      author: 'AJD862',
+      post: 'AJDK9534',
+    },
+    {
+      id: 'C-992068',
+      text: 'Charlie wins ALL',
+      author: 'AJD862',
+      post: 'AJDK9534',
+    },
+    {
+      id: 'C-992069',
+      text: 'Charlie wins ALL',
+      author: 'AJD862',
+      post: 'AJDK98u38',
+    },
+    {
+      id: 'C-99206',
+      text: 'Charlie wins ALL',
+      author: 'AJD862',
+      post: 'AJDK98u38',
+    },
+    {
+      id: 'C-996311',
+      text: 'Couldnt do it without Alan',
+      author: 'AJD862',
+      post: 'AJDK98u38',
+    },
+  ];
+
+  export {users, posts, comments};
--- src/index.js
+++ src/index.js
@@ -1,36 +1,157 @@
-import { GraphQLServer } from 'graphql-yoga'
+import {GraphQLServer} from 'graphql-yoga';
+import { users, posts, comments } from './data';
 
 // typeDefs
 // Scalar - String, Boolean, Int, Float, ID
 
+// sample data
+
 const typeDefs = `
     type Query {
+        greeting(name: String): String!
+        add(numbers: [Float!]!): Float!
+        grades: [Int!]!
+        me: User!
+        post: Post!
+        users(query: String): [User!]!
+        posts(query: String): [Post!]!
+        comments: [Comment!]!
+    }
+
+    type Mutation {
+        createUser(name: String!, email: String!, age:Int): User!
+    }
+
+    type Comment {
+        id: ID!
+        text: String!
+        author: User!
+        post: Post
+    } 
+
+    type User {
         id: ID!
         name: String!
-        employed: Boolean!
+        email: String!
         age: Int!
         gpa: Float
-    }`;
+        isEmployed: Boolean!
+        posts: [Post!]!
+        comments: [Comment!]!
+    }
+
+    type Post {
+        id: ID!
+        title: String!
+        body: String!
+        published: Boolean!
+        author: User!
+        comments: [Comment!]!
+    }
+    `;
 
 const resolvers = {
-    Query: {
-        id() {
-            return '34as498lo';
-        },
-        name() {
-            return 'Aakash Ohri';
-        },
-        employed() {
-            return true;
-        },
-        age() {
-            return 30;
-        },
-        gpa() {
-            return 8.56;
-        }
+  Query: {
+    users(parent, args, ctx, info) {
+      if (!args.query) {
+        return users;
+      } else {
+        return users.filter(user =>
+          user.name.toLocaleLowerCase().includes(args.query.toLocaleLowerCase()),
+        );
+      }
+    },
+    posts(parent, args, ctx, info) {
+      if (!args.query) {
+        return posts;
+      } else {
+        const titleInPosts = posts.filter(post =>
+          post.title.toLocaleLowerCase().includes(args.query),
+        );
+        const bodyInPosts = posts.filter(post =>
+          post.title.toLocaleLowerCase().includes(args.query),
+        );
+        return titleInPosts || bodyInPosts;
+      }
+    },
+
+    comments(parent, args, ctx, info) {
+      return comments;
+    },
+    greeting(parent, args, ctx, info) {
+      console.log('args::', args);
+      return `Hello my name is ${args.name}`;
+    },
+    grades() {
+      return [4, 9, 1];
+    },
+    add(parent, args, ctx, info) {
+      if (args.numbers.length === 0) {
+        return 0;
+      } else {
+        return args.numbers.reduce((acc, curr) => acc + curr);
+      }
+    },
+    me() {
+      return {
+        id: 'ASHFY6453',
+        name: 'Aakash Ohri',
+        age: 30,
+        gpa: null,
+        isEmployed: true,
+      };
+    },
+  },
+  Mutation : {
+      createUser(parent, args, ctx, info) {
+            const isUserExist = users.some(user => user.email === args.email);
+            if(isUserExist) {
+                throw new Error('User Already Exist');
+            }
+
+            let user = {
+                id: 'A903726',
+                email: args.email,
+                age: args.age,
+                name: args.name,
+            }
+            users.push(user);
+
+            return user;
+      }
+  },
+  Post: {
+    author(parent, args, ctx, info) {
+      // from every post object it extracts out author key
+      return users.find(user => user.id === parent.author);
+    },
+    comments(parent, args, ctx, info) {
+        return comments.filter(comment => {
+            return parent.comments.includes(comment.id);
+        })
     }
-}
+  },
+  User: {
+    posts(parent, args, ctx, info) {
+      // so from data
+      return posts.filter(post => post.author === parent.id);
+    },
+    comments(parent, args, ctx, info) {
+        //parent.comments
+        return comments.filter(comment => {
+            return parent.comments.includes(comment.id);
+        })
+    },
+  },
+  Comment: {
+    author(parent, args, ctx, info) {
+      return users.find(user => user.id === parent.author);
+    },
+    post(parent, args, ctx, info) {
+        return posts.find(post => post.id === parent.post);
+    }
+  },
+};
 
 const server = new GraphQLServer({typeDefs, resolvers});
-server.start(() => console.log('Server is running on localhost:4000'))
+server.start(() => console.log('Server is running on localhost:4000'));
